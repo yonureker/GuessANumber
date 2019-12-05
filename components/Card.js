d563c53023096c4c9a4a58cac4686ac1349c81fe
yonureker <yonureker@gmail.com>
2019-12-04 21:54:31 -0800

    update Card component

--- components/Card.js
+++ components/Card.js
@@ -2,14 +2,11 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Card = props => {
-  return <View>{props.children}</View>;
+  return <View style={{...styles.card, ...props.style}}>{props.children}</View>;
 };
 
 const styles = StyleSheet.create({
   card: {
-    width: 300,
-    maxWidth: "80%",
-    alignItems: "center",
     shadowColor: "black",
     shadowOffset: {
       width: 0,
--- components/screens/StartGame.js
+++ components/screens/StartGame.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import Card from '../Card'
 
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -34,16 +35,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    backgroundColor: "white",
-    //shadow for Android
-    elevation: 5
   },
   title: {
     fontSize: 20,
